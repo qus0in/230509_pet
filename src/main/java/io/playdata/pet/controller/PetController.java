666b7a55221ccd92f12cbbdecf5d738bd557d104
qus0in <qus0in@gmail.com>
2023-05-09 16:50:57 +0900

    파일 업로드 구현

--- src/main/java/io/playdata/pet/controller/PetController.java
+++ src/main/java/io/playdata/pet/controller/PetController.java
@@ -1,10 +1,17 @@
 package io.playdata.pet.controller;
+import io.playdata.pet.model.Pet;
 
 import io.playdata.pet.service.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 public class PetController {
@@ -14,6 +21,27 @@ public class PetController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("msg", "빨리 빨리 등록하세요!");
+        model.addAttribute("pets", petService.getAllPets());
         return "index";
+        // http://localhost:8080
+    }
+
+    @GetMapping("/add")
+    public String add(Model model) {
+        model.addAttribute("pet", new Pet());
+        // add.html에 form을 만들건데, 그 form에 그릇이 될 빈 객체를 전달
+        return "add"; // static/templates/add.html
+        // http://localhost:8080/add
+    }
+
+    @PostMapping("/add")
+    public String add(
+            @ModelAttribute("pet") Pet pet,
+            @RequestParam("imageFile") MultipartFile imageFile,
+            @RequestParam("voiceFile") MultipartFile voiceFile,
+            Model model
+    ) throws IOException {
+        petService.savePet(pet, imageFile, voiceFile);
+        return "redirect:/";
     }
 }
--- src/main/java/io/playdata/pet/service/PetService.java
+++ src/main/java/io/playdata/pet/service/PetService.java
@@ -3,8 +3,12 @@ package io.playdata.pet.service;
 import io.playdata.pet.model.Pet;
 import io.playdata.pet.repository.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -32,5 +36,34 @@ public class PetService {
 
     public void deletePet(Long id) {
         petRepository.deleteById(id);
+    }
+
+    @Value("${upload.path}") // application.properties에 지정
+    private String uploadPath;
+
+    // 메소드 오버로딩 (패러미터를 다르게 해서 다른 것 취급)
+    public void savePet(Pet pet, MultipartFile imageFile, MultipartFile voiceFile) throws IOException {
+        // imageFile이 비어있지 않으면
+        if (imageFile != null) {
+            // 겹칠 수 있기 때문에 현재 시스템의 시간(currentTimeMillis)과 파일명을 합쳐서
+            // 새로운 파일이름(fileName) 만들기
+            String fileName = System.currentTimeMillis() + "-" + imageFile.getOriginalFilename();
+            // 업로드를 받을 폴더 경로와 파일명을 합쳐서, 새로운 파일이 들어갈 최종 경로
+            File file = new File(uploadPath + "/" + fileName);
+            imageFile.transferTo(file); // transferTo -> 전송받은 imageFile을 file 위치에 생성
+            pet.setImage(fileName); // 그 파일 이름을 db에 저장하기 위해 image 속성에 파일명을 넣습니다
+        }
+        // voiceFile이 비어있지 않으면
+        if (voiceFile != null) {
+            // 겹칠 수 있기 때문에 현재 시스템의 시간(currentTimeMillis)과 파일명을 합쳐서
+            // 새로운 파일이름(fileName) 만들기
+            String fileName = System.currentTimeMillis() + "-" + voiceFile.getOriginalFilename();
+            // 업로드를 받을 폴더 경로와 파일명을 합쳐서, 새로운 파일이 들어갈 최종 경로
+            File file = new File(uploadPath + "/" + fileName);
+            voiceFile.transferTo(file); // transferTo -> 전송받은 voiceFile을 file 위치에 생성
+            pet.setVoice(fileName); // 그 파일 이름을 db에 저장하기 위해 voice 속성에 파일명을 넣습니다
+        }
+
+        petRepository.save(pet);
     }
 }
